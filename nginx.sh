1167fde3c866112c9cede1d7ccc19097056d7091
Ilkin <ilkin.mammadovv@gmail.com>
2021-07-29 17:49:36 +0400

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+nginx=stable # use nginx=development for latest development version
+echo -ne '\n' | add-apt-repository ppa:nginx/$nginx
+apt-get update -y
+echo -ne '\n' | apt-get install nginx -y
