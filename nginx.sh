687a87a024ac3e6c0a23b557b5b0ff596a5532ce
Ilkin <ilkin.mammadovv@gmail.com>
2021-09-01 15:44:18 +0400

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nginx=stable # use nginx=development for latest development version
-echo -ne '\n' | add-apt-repository ppa:nginx/$nginx
+echo -ne '\n' | add-apt-repository ppa:nginx/$nginx   #need_check_repos
 apt-get update -y
 echo -ne '\n' | apt-get install nginx -y
